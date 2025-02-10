0535786f4e3699604354f4d27d1014dbb9ce7536
drondiz96 <drondiz9966@gmail.com>
2025-02-10 23:29:45 +0300

    add_loop

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
+def loop(n : int):
+        for i in range(n):
+            print(i)
+            
 def main():
     print("Hello World!")
+    loop(3)
     return
-
+    
 if __name__ == "__main__":
     main()
+
