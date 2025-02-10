1a2e68540a576307c5ec3009ddf875cbbce0e4d3
drondiz96 <drondiz9966@gmail.com>
2025-02-10 23:33:08 +0300

    add_loop2

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 def loop(n : int):
-        for i in range(n):
-            print(i)
-            
+    for i in range(n):
+        print(i)
+
+def loop2(n : int):
+    for i in range(n):
+        print(i)
+
 def main():
     print("Hello World!")
     loop(3)
+    loop2(4)
     return
     
 if __name__ == "__main__":
     main()
-
