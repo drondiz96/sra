c8c01cce200e9ff6da8752c60e09ddd5ffea81cd
danil-karzhavin <karzhavin.danil@mail.ru>
2025-03-14 02:19:18 +0300

    Реализована двухфакторная аутентификация через почту

--- src/main/java/bip/bip_project/controller/user/UserController.java
+++ src/main/java/bip/bip_project/controller/user/UserController.java
@@ -2,10 +2,13 @@ package bip.bip_project.controller.user;
 
 import bip.bip_project.model.authenticate.AuthenticationRequest;
 import bip.bip_project.model.authenticate.AuthenticationResponse;
+import bip.bip_project.model.authenticate.Verify2faRequest;
 import bip.bip_project.model.user.User;
 import bip.bip_project.model.user.UserDto;
 import bip.bip_project.security.JwtUtil;
 import bip.bip_project.service.user.IUserService;
+import bip.bip_project.service.user.TwoFactorAuthService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,10 +19,12 @@ import java.util.Map;
 public class UserController {
     IUserService userService;
     JwtUtil jwtUtil;
+    TwoFactorAuthService twoFactorAuthService;
 
-    public UserController(IUserService userService, JwtUtil jwtUtil) {
+    public UserController(IUserService userService, JwtUtil jwtUtil, TwoFactorAuthService twoFactorAuthService) {
         this.userService = userService;
         this.jwtUtil = jwtUtil;
+        this.twoFactorAuthService = twoFactorAuthService;
     }
 
     @PostMapping("/createUser")
@@ -50,8 +55,24 @@ public class UserController {
     @PostMapping("/authenticate")
     public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
         final User user = userService.identicateAndAuthenticate(authenticationRequest.getEmail(), authenticationRequest.getPassword());
-        final String jwt = jwtUtil.generateToken(user);
+        String code = twoFactorAuthService.generateCode();
+        twoFactorAuthService.sendCode(user.getEmail(), code);
+        twoFactorAuthService.storeCode(user.getEmail(), code);
 
-        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+        return ResponseEntity.ok(new AuthenticationResponse("2FA code sent to your email"));
+    }
+
+    @PostMapping("/verify-2fa")
+    public ResponseEntity<?> verify2fa(@RequestBody Verify2faRequest verify2faRequest) {
+        String email = verify2faRequest.getEmail();
+        String code = verify2faRequest.getCode();
+
+        if (twoFactorAuthService.verifyCode(email, code)) {
+            User user = userService.getUserByEmail(email);
+            final String jwt = jwtUtil.generateToken(user);
+            return ResponseEntity.ok(new AuthenticationResponse(jwt));
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid 2FA code");
+        }
     }
 }
