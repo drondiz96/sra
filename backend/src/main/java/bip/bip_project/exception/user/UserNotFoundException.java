41c0ba6f0a05c8e0025d5fa5427506454886da89
drondiz96 <drondiz96@gmail.com>
2025-05-13 17:06:05 +0300

    delete back

--- backend/src/main/java/bip/bip_project/exception/user/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package bip.bip_project.exception.user;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message){
-        super(message);
-    }
-}
--- backend/src/main/java/bip/bip_project/model/user/UserRole.java
+++
@@ -1,6 +0,0 @@
-package bip.bip_project.model.user;
-
-public enum UserRole {
-    ROLE_USER,
-    ROLE_ADMIN;
-}
