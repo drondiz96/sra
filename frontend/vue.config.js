8b7731bae65681eba22620de04b213bac7e81120
Dron139 <moshenkov2012@gmail.com>
2025-06-04 16:16:06 +0300

    добавил домен phonereview

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+
+  devServer: {
+    host: 'phonereview',
+    port: 8081,
+    allowedHosts: 'all',
+
+    setupMiddlewares(middlewares, devServer) {
+      // отключаем проверку Host-заголовка
+      devServer.app.use((req, res, next) => {
+        req.headers.host = 'localhost'; // подменяем заголовок
+        next();
+      });
+      return middlewares;
+    }
+  }
+});
